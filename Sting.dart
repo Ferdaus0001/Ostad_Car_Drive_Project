6d5f8d45b2a6fe9874e35e9253462e639830c9c4
ferdaus_hossan <ferdaushossan7788990@gmail.com>
2024-06-13 22:12:24 +0600

    small project

---
+++ Sting.dart
@@ -0,0 +1,97 @@
+/*
+void main (){
+  String? firstNlaem = "ferdaush ";
+  String? LastName = "hossan";
+ print("$firstNlaem$LastName");
+  var name1 = 'hi ';
+  var name2 = "HI ";
+  print("$name1$name2");
+  int number = 7;
+  print(number);
+int number1 = 7;
+int numb = 90;
+print(numb+number1);
+
+
+}*/
+// import 'dart:async';
+//
+// void main (){
+//   int num =1;
+//   String?name = 'ferdahuas ';
+//   double numa= 45.4;
+//   print(num.runtimeType);
+//
+// }
+//
+//
+
+
+// this is code for not work
+
+
+
+
+
+  // print("This is hello World  code ");
+  // String name 'ferahsu ';
+  // for(i = 1; i <= 50 ; naem ++ )
+  // // hollow word code
+//
+// void main (){
+//   String? naem  = 'ferhaud ';
+//   for(String?i = 0;i name >= 5 ; i ++ ){
+//     print(naem);
+//   }
+//   }
+
+// import 'dart:io';
+// void main(){
+//   print("Enter Your Number ");
+//   int?number =int.parse(stdin.readLineSync()!);
+//   print("number is ${number}");
+//
+// }
+// void main (){
+//   int total = 0;
+//   int n = 5;// change as per required
+//   for(int i =1; i <= n; i++){
+//
+//     total= total + i;
+//
+//   }
+//   print("Total is ${total}");
+// }
+// void main(){
+// //   int num1 = 100;
+// //   double num2 = 120.2;
+// //   num num3  =50;
+// //   num num4= 50.54;
+// //   num sum= num1  + num2+ num3 + num4;
+// //   print("$num1");
+// //   print("$num2");
+// //   print("$num3");
+// //   print("$num4");
+// //   print("$sum");
+// //
+// // }
+// void main(){
+//   List<String>naem = ["f","4","5","5r",];
+//   print("Thsi is a ${naem[3]}");
+//   print("Thsi is a $naem");
+//   print("Thsi is a $naem");
+//   print("Thsi is a $naem");
+import 'dart:io';
+
+void main() {
+
+  for(var i=1;i<=5;i++)
+  {
+    for(var j=1;j<=5;j++)
+    {
+      stdout.write("*");
+    }
+    print("tg");
+  }
+
+}
