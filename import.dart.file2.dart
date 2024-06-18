e8db84ecee1d201168ecd8253f66c3f69d5a36e0
ferdaus_hossan <ferdaushossan7788990@gmail.com>
2024-06-18 08:14:08 +0600

    small project

---
+++ import.dart.file2.dart
@@ -0,0 +1,43 @@
+
+//   import 'dart:io';
+// void main(){
+//   print(" What is Yoru Name ");
+//   String? naem = stdin.readLineSync();
+//   print(" Thank You $naem");
+//   stdout.write("Enter Yoru Number  Sir ");
+//   int number = int.parse(stdin.readLineSync()!);
+//   print(" Your Number is $number");
+//   stdout.write(" What is Your Number2 ");
+//   int number2 = int.parse(stdin.readLineSync()!);
+//   print(" Your number1 is $number2");
+//   stdout.write("Enter Yoru GPA sir ");
+//   double gap = double.parse(stdin.readLineSync()!);
+//   print(" O Yoru GPA is $gap");
+//   print(" Thank You $naem Sir for Your Information ");
+//
+//
+// }
+// import 'dart:io';
+// void main(){
+//   stdout.write("Etner Yoru Lop Type 0 for ster Sir  ");
+//   int loop = int.parse(stdin.readLineSync()!);
+//   int a = 45;
+//   double c = 45.5;
+//   Map map  = {
+//     " Id ": 4545, " Name ": "ferdaush", " city ": ' jessore ',  " coutnry ":   " bangladesh ",
+//   };
+//   String b = ' ferdauhs ';
+//     for( int i = 2; i<=  50; i ++ ){
+//       print(" Lop $i  $a $b $c $map * $map $map * $map");
+//       print('Your Loop is End Sir ');
+//     }
+
+// }
+void main(){
+  List<int> Number = [45,45,45,45,4,5,5];
+  List<String> Name = [ 'feraduah ', " hossand 1", " sojfib ", " ridka e", ];
+for(int i  = 1; i <= 45; i ++ ){
+  print("||||| ................  this is a List Sir $i $Number  $Name................ |||||");
+}
+
+}
